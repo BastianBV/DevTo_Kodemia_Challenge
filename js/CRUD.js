4e1cd999fbf4c347d82b483325c666ef04646663
Rods <rodolfo.perez01@iest.edu.mx>
2022-08-01 21:20:31 -0500

    Correciones

--- js/CRUD.js
+++ js/CRUD.js
@@ -8,103 +8,103 @@ let postsHolder = document.querySelector('.postsHolder')
 console.log()
 
 
-const insertPost = (post) => {
+// const insertPost = (post) => {
 
-    let template = ''
+//     let template = ''
     
-    for (post in postToRender) {
-        template += `
-        <div class="card-body">
+//     for (post in postToRender) {
+//         template += `
+//         <div class="card-body">
 
-        <div class="d-flex">
-            <div>
-                <a href="">
-                    <img src="/images/icon_profile.jpg" alt="" class="profile-dimention">
-                </a>
-            </div>
+//         <div class="d-flex">
+//             <div>
+//                 <a href="">
+//                     <img src="/images/icon_profile.jpg" alt="" class="profile-dimention">
+//                 </a>
+//             </div>
 
-            <div class="profile-card">
-                <span class="name-profile">Lorem ipsum </span>
-                <span class="date-profile">Lorem ipsum</span>
-            </div>
-        </div>
+//             <div class="profile-card">
+//                 <span class="name-profile">Lorem ipsum </span>
+//                 <span class="date-profile">Lorem ipsum</span>
+//             </div>
+//         </div>
 
-        <div class="card-distribution">
-            <h2 class="card-title">
-                <a href="entry.html" class="title-post">
-                    People rarely use complicated git commands on the weekend, and I have
-                    the
-                    data
-                    to prove
-                    it.
+//         <div class="card-distribution">
+//             <h2 class="card-title">
+//                 <a href="entry.html" class="title-post">
+//                     People rarely use complicated git commands on the weekend, and I have
+//                     the
+//                     data
+//                     to prove
+//                     it.
 
-                </a>
+//                 </a>
 
-            </h2>
-            <div>
-                <a class="card-text">
-                    <span class="style-cat-js">#</span>javascript
-                </a>
-                <a class="card-text-html">
-                    <span class="style-cat-html">#</span>html
-                </a>
-                <a class="card-text-react">
-                    <span class="style-cat-react">#</span>react
-                </a>
-            </div>
+//             </h2>
+//             <div>
+//                 <a class="card-text">
+//                     <span class="style-cat-js">#</span>javascript
+//                 </a>
+//                 <a class="card-text-html">
+//                     <span class="style-cat-html">#</span>html
+//                 </a>
+//                 <a class="card-text-react">
+//                     <span class="style-cat-react">#</span>react
+//                 </a>
+//             </div>
             
-            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+//             <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
 
-            <div class="d-flex justify-content-between icons-general">
-                <div>
-                    <a href="" class="edition-icon">
-                        <img src="/images/like.png" alt="">
-                        <span class="visibility-title-icon">
-                            reactions
-                        </span>
-                    </a>
-                    <a href="" class="edition-icon">
-                        <img src="/images/comment.png" alt="">
-                        <span class="visibility-title-icon">
-                            comments
-                        </span>
+//             <div class="d-flex justify-content-between icons-general">
+//                 <div>
+//                     <a href="" class="edition-icon">
+//                         <img src="/images/like.png" alt="">
+//                         <span class="visibility-title-icon">
+//                             reactions
+//                         </span>
+//                     </a>
+//                     <a href="" class="edition-icon">
+//                         <img src="/images/comment.png" alt="">
+//                         <span class="visibility-title-icon">
+//                             comments
+//                         </span>
 
-                    </a>
-                </div>
-                <div>
-                    <small class="read-time">16 min read</small>
-                    <button class="btn btn-secondary button-save">Save</button>
-                </div>
-            </div>
+//                     </a>
+//                 </div>
+//                 <div>
+//                     <small class="read-time">16 min read</small>
+//                     <button class="btn btn-secondary button-save">Save</button>
+//                 </div>
+//             </div>
 
-        </div>
+//         </div>
 
 
-    </div>
-</div>
-`
-            }
+//     </div>
+// </div>
+// `
+//             }
             
-            postsHolder.innerHTML = template
-        }
+//             postsHolder.innerHTML = template
+//         }
         
         
         
         
         
-        fetch('https://devto-javascript-default-rtdb.firebaseio.com/.json')
-        .then((post)=>{
+        // fetch('https://devto-javascript-default-rtdb.firebaseio.com/.json')
+        // .then((post)=>{
             
-            return post.json()
-        })
-        .then((post)=>{
+        //     return post.json()
+        // })
+        // .then((post)=>{
             
-            insertPost(post)
-        })
+        //     insertPost(post)
+        // })
         
-        .catch((error)=>{
-            console.log(error)
-        })
+        // .catch((error)=>{
+        //     console.log(error)
+        // })
 
 
         

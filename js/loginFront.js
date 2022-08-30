bb390239bc0d7f007dabae4c58cba523f1f0eeba
pedropalmeros <pedrofpalmeros@gmail.com>
2022-08-30 17:56:38 -0500

    [add] loginFront

---
+++ js/loginFront.js
@@ -0,0 +1,46 @@
+console.log('Hola');
+
+const loginBtnDOM = document.querySelector(".loginBtn");
+const passwordInput = document.querySelector(".passwordInput");
+const emailInput = document.querySelector(".emailInput");
+
+let passwordLogin = '';
+let  emailLogin  = '';
+const postToInsert = {}
+
+const urlBackLogin = 'http://localhost:8080/auth'
+
+
+loginBtnDOM.addEventListener("click",(e)=>{
+    passwordLogin = document.querySelector(".passwordInput").value;
+    emailLogin = document.querySelector(".emailInput").value;
+
+    if( passwordLogin.length > 0 && emailLogin.length > 0)
+    {   
+        postToInsert["email"] = emailLogin;
+        postToInsert["password"] = passwordLogin;
+
+        fetch(urlBackLogin,{
+            method: 'POST',
+            body: JSON.stringify(postToInsert),
+            headers: {'content-type': 'application/json; charset=UTF-8'}
+        })        
+        .then((response)=>response.json())
+        .then((json)=>{
+            console.log(json);
+            console.log("state",json.success);
+        
+            if(!json.success ){
+                alert("Correo y/o contraseña incorrectos")
+            }else{
+                localStorage.setItem("token",json.data.token);
+                window.location.href='index.html';
+            }
+        })
+        .catch(err => console.log(err));
+    }else{
+        alert("Correo y contraseña son obligatorios");
+    }
+})
+
+

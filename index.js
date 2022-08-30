d54350439e213f32a6cb75733012dafd9bb96e20
pedropalmeros <pedrofpalmeros@gmail.com>
2022-08-30 17:48:13 -0500

    [++] index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hola");
+
+const LogInBtn =  document.querySelector(".LogInBtn");
+const CreatePostBtn =  document.querySelector(".CreatePostBtn");
+
+let keysInLocal = window.localStorage
+
+console.log(keysInLocal)
+
+if(keysInLocal.token){//token exists
+    LogInBtn.classList.add('d-none');
+    CreatePostBtn.classList.remove('d-none');
+}
+else{
+    LogInBtn.classList.remove('d-none');
+    CreatePostBtn.classList.add('d-none');
+}

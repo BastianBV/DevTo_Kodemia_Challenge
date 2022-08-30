7e8361d8410c48628efe95ec088c1676a44173f0
pedropalmeros <pedrofpalmeros@gmail.com>
2022-08-30 18:11:08 -0500

    [modify] buttons

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ console.log(keysInLocal)
 console.log(keysInLocal)
 
 if(keysInLocal.token){//token exists
+    console.log('hay token')
     LogInBtn.classList.add('d-none');
     CreatePostBtn.classList.remove('d-none');
 }
 else{
+    console.log('no hay token');
     LogInBtn.classList.remove('d-none');
     CreatePostBtn.classList.add('d-none');
 }
